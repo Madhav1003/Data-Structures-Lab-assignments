40a28f362b602a1b001865a620bfc9aea4a6d225
Madhav1003 <mmahajan_be24@thapar.edu>
2025-08-21 11:47:47 +0530

    Add files via upload

---
+++ Assignment_2/additional_q2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool sol(string str){
+    int arr[26] = {0};
+    for (int i = 0; i < str.size(); i++)
+    {
+        arr[str[i] - 'a']++;
+    }
+    for (int i = 0; i < str.size(); i++)
+    {
+        if(arr[i]==3) return true;
+    }
+    return false;
+}
+
+int main(){
+
+    string str = "abcd";
+    cout<<sol(str);
+
+}
