a2e7c9f395133cd87c43af643a2e4f36a2b54bc3
Madhav1003 <mmahajan_be24@thapar.edu>
2025-11-09 16:46:44 +0530

    Update Q4.cpp
    Refactored the linked list implementation and added a new function to ensure the previous pointers are correctly set.

--- Assignment_6/Q4.cpp
+++ Assignment_6/Q4.cpp
@@ -1,67 +1,19 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-class node
-{
+class Node{
 public:
-    char data;
-    node *next = NULL;
-    node *prev = NULL;
+int d;
+Node* next = NULL;
+Node* prev = NULL;
 };
 
-class dll
-{
-    node *head = NULL;
-
-public:
-    void at_head(char x)
-    {
-        node *newn = new node;
-        if (!head)
-        {
-            head = newn;
-            head->data = x;
-            return;
-        }
-        newn->next = head;
-        head->prev = newn;
-        head = newn;
-        newn->data = x;
+Node* sol(Node* head){
+    if(!head) return head;
+    Node* temp = head;
+    while(temp->next){
+        if(temp->next->prev!=temp) temp->next->prev = temp;
+        temp=temp->next;
     }
-    void display()
-    {
-        node *temp = head;
-        while (temp)
-        {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-        cout << endl;
-    }
-    bool isplaindrome(){
-        if(!head) return false;
-        if(head->next==NULL) return true;
-        node* end=head;
-        while(end->next!=NULL){
-            end=end->next;
-        }
-        node* front =head;
-        while(front!=end){
-            if(tolower(front->data)!=tolower(end->data)) return false;
-            front=front->next;
-            end=end->prev;
-        }
-        return true;
-    }
-};
-int main() {
-
-    dll d1;
-    d1.at_head('L');
-    d1.at_head('E');
-    d1.at_head('V');
-    d1.at_head('E');
-    d1.at_head('l');
-    d1.display();
-    cout<<d1.isplaindrome();
+    return head;
 }
