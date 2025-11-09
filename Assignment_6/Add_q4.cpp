e52b57b84f8bdd9dc98885d5c71ede33c37ac949
Madhav1003 <mmahajan_be24@thapar.edu>
2025-11-09 16:49:20 +0530

    Update Add_q4.cpp
    Refactored the linked list node removal functions and added a new Node class definition.

--- Assignment_6/Add_q4.cpp
+++ Assignment_6/Add_q4.cpp
@@ -1,72 +1,19 @@
-int parity(int n) {
-    int count = 0;
-    while (n) {
-        count += (n & 1);
-        n >>= 1;
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node* next = NULL;
+Node* prev = NULL;
+};
+
+Node* sol(Node* head){
+    if(!head) return head;
+    Node* temp = head;
+    while(temp->next){
+        if(temp->next->prev!=temp) temp->next->prev = temp;
+        temp=temp->next;
     }
-    return count;
-}
-
-Node* removeEvenParityNodes_CLL(Node* head) {
-    if (!head) return NULL;
-
-    Node* curr = head;
-    Node* prev = NULL;
-
-    do {
-        if (parity(curr->data) % 2 == 0) {
-            if (curr == head) {
-                Node* last = head;
-                while (last->next != head)
-                    last = last->next;
-                if (last == head) {
-                    delete head;
-                    return NULL;
-                }
-
-                head = head->next;
-                last->next = head;
-                Node* temp = curr;
-                curr = head;
-                delete temp;
-            } else {
-                prev->next = curr->next;
-                Node* temp = curr;
-                curr = curr->next;
-                delete temp;
-            }
-        } else {
-            prev = curr;
-            curr = curr->next;
-        }
-    } while (curr != head);
-return head;
-}
-
-
-Node* removeEvenParityNodes_DLL(Node* head) {
-    if (!head) return NULL;
-
-    Node* curr = head;
-
-    while (curr) {
-        Node* nextNode = curr->next;
-
-        if (parity(curr->data) % 2 == 0) { 
-            if (curr == head) {
-                head = curr->next;
-                if (head) head->prev = NULL;
-                delete curr;
-            }
-            else {
-                if (curr->prev) curr->prev->next = curr->next;
-                if (curr->next) curr->next->prev = curr->prev;
-                delete curr;
-            }
-        }
-
-        curr = nextNode;
-    }
-
     return head;
 }
