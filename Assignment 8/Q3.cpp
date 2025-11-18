9c441843b37c91114ac79055d75453526dfaf76e
Madhav1003 <mmahajan_be24@thapar.edu>
2025-11-19 01:59:14 +0530

    Update Q3.cpp
    Add a structure for a binary tree node and a function to calculate its maximum depth.

--- Assignment 8/Q3.cpp
+++ Assignment 8/Q3.cpp
@@ -1,1 +1,14 @@
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
+
+
+int maxDepth(Node* root) {
+    if (!root) return 0;
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+
 
