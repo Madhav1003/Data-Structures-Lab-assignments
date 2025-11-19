2491b7e0bf00594b76609d9d33601211e97c58d7
Madhav1003 <mmahajan_be24@thapar.edu>
2025-11-19 09:43:06 +0530

    Update Q3.cpp
    Added functions to find minimum value and delete a node in a binary tree.

--- Assignment 8/Q3.cpp
+++ Assignment 8/Q3.cpp
@@ -5,7 +5,38 @@ struct Node {
     Node(int val) : data(val), left(NULL), right(NULL) {}
 };
 
+int findMin(Node* root) {
+    if (!root) return -1;
+    while (root->left) root = root->left;
+    return root->data;
+}
 
+node* deltetion(node* root,int x){
+    if(!root) return root;
+    if(root->data == x){
+        // if 0 child
+        if(!root->left && !root->right){
+            delete root;
+            return NULL;
+        }
+            if(root->left && !root->right){
+                node* temp = root->left;
+                delete root;
+                return temp;
+            }
+            if(root->right && !root->left){
+                node* temp = root->right;
+                delete root;
+                return root->right;
+            }
+        if(root->left && root->right){
+            int min = findMin(root);
+            root->right = deltetion(root->right,min);
+        }
+    }
+    else if (root->data < x) root->left = deltetion(root->right,x);
+    else root->right = deltetion(root->right,x);
+}
 
 int maxDepth(Node* root) {
     if (!root) return 0;
