df4eab941d87fc3e194d1810bba5e6cb1efad6de
Madhav1003 <mmahajan_be24@thapar.edu>
2025-11-19 09:48:48 +0530

    Update Q3.cpp
    Added a function to calculate the minimum depth of a binary tree.

--- Assignment 8/Q3.cpp
+++ Assignment 8/Q3.cpp
@@ -42,4 +42,15 @@ int maxDepth(Node* root) {
     if (!root) return 0;
     return 1 + max(maxDepth(root->left), maxDepth(root->right));
 
+int minDepth(Node* root) {
+    if (!root) return 0;
+    if (!root->left && !root->right)
+        return 1;
+    if (!root->left)
+        return 1 + minDepth(root->right);
+    if (!root->right)
+        return 1 + minDepth(root->left);
+
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+}
 
