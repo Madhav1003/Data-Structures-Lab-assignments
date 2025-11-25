bd7c30d4d4b697f6566499b9463d8a741444bf7b
Madhav1003 <mmahajan_be24@thapar.edu>
2025-11-26 03:38:23 +0530

    Update Q4.cpp
    Added a Node structure for binary tree implementation.

--- Assignment 8/Q4.cpp
+++ Assignment 8/Q4.cpp
@@ -1,3 +1,10 @@
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
 bool isBSTUtil(Node* root, int minVal, int maxVal) {
     if (root == NULL)
         return true;
